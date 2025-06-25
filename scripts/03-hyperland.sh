a7b62abd6cd84716469a2b4fc680fd7438dcc58d
DanteLorenzo <n1emand4me@gmail.com>
2025-06-25 14:57:55 +0300

    hyprland script

---
+++ scripts/03-hyperland.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Include logging functions
+source "$(dirname "$0")/../utils/logging.sh"
+
+log_info "Installing Hyprland and dependencies with pacman..."
+sudo pacman -S --needed --noconfirm \
+  base-devel hyprland hyprpaper waybar gdb ninja gcc cmake libxcb xcb-proto xcb-util \
+  xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender \
+  pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm \
+  xorg-xwayland cmake wlroots mesa git meson polkit
+if [ $? -eq 0 ]; then
+  log_success "Hyprland and dependencies installation complete."
+else
+  log_error "Hyprland and dependencies installation failed."
+fi
+
--- scripts/99-hyperland.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Include logging functions
-source "$(dirname "$0")/../utils/logging.sh"
-
