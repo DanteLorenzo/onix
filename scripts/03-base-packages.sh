05fc19f11cc4e28f660c2dc0512f9c88dd2f49ed
DanteLorenzo <n1emand4me@gmail.com>
2025-06-25 23:57:10 +0300

    timeshift

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -4,7 +4,7 @@
 source "$(dirname "$0")/../utils/logging.sh"
 
 log_info "Installing base packages with pacman..."
-sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon
+sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift
 
 if [ $? -eq 0 ]; then
   log_success "Base packages installation complete."
