31ef4e9f19b74b42cf9e1accafcc5ddba20ac032
DanteLorenzo <n1emand4me@gmail.com>
2025-06-25 12:26:58 +0300

    fonts + change theme

--- scripts/01-grub.sh
+++ scripts/01-grub.sh
@@ -12,7 +12,7 @@ sudo cp ./fonts/* /boot/grub/fonts/
 
 # Копируем тему GRUB
 log_info "Copying GRUB theme configuration..."
-sudo cp ./configs/grub/ameli_theme.txt /boot/grub/themes/
+sudo cp ./configs/grub/AmeliTheme.txt /boot/grub/themes/
 
 # Редактируем /etc/default/grub
 log_info "Updating /etc/default/grub with new theme and font..."
@@ -20,8 +20,8 @@ sudo cp /etc/default/grub /etc/default/grub.bak
 sudo awk '!/^GRUB_THEME/ && !/^GRUB_FONT/' /etc/default/grub.bak | \
   sudo tee /etc/default/grub > /dev/null
 
-echo 'GRUB_THEME="/boot/grub/themes/ameli_theme.txt"' | sudo tee -a /etc/default/grub > /dev/null
-echo 'GRUB_FONT="/boot/grub/fonts/terminus-18.pf2"' | sudo tee -a /etc/default/grub > /dev/null
+echo 'GRUB_THEME="/boot/grub/themes/AmeliTheme.txt"' | sudo tee -a /etc/default/grub > /dev/null
+echo 'GRUB_FONT="/boot/grub/fonts/terminus-28.pf2"' | sudo tee -a /etc/default/grub > /dev/null
 
 # Обновляем GRUB
 log_info "Updating GRUB..."
