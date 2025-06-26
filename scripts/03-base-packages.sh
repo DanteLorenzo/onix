6580c6c0d0e14849e2d1cda52f3e273eafdb553a
DanteLorenzo <n1emand4me@gmail.com>
2025-06-26 22:05:58 +0300

    fix

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -5,10 +5,10 @@ source "$(dirname "$0")/../utils/logging.sh"
 
 # Base
 log_info "Installing base packages with pacman..."
-sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift firefox pipewire wireplumber pipewire-pulse pipewire-alsa xorg-xkbcomp xorg-xkbcommon jq
+sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift firefox pipewire wireplumber pipewire-pulse pipewire-alsa xorg-xkbcomp jq
 
 # Fonts
-sudo pacman -S --noconfirm ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
+sudo pacman -S --needed --noconfirm ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
 
 if [ $? -eq 0 ]; then
   log_success "Base packages installation complete."
