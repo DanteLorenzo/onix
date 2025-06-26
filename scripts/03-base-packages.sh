b13d1398e9c5bb18493089daae0c919da3043122
DanteLorenzo <n1emand4me@gmail.com>
2025-06-26 20:49:12 +0300

    fix

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -3,8 +3,12 @@
 # Include logging functions
 source "$(dirname "$0")/../utils/logging.sh"
 
+# Base
 log_info "Installing base packages with pacman..."
 sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift
+
+# Fonts
+sudo pacman -S ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
 
 if [ $? -eq 0 ]; then
   log_success "Base packages installation complete."
