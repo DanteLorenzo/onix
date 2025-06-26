f6b6fb723a2e208fbbdc318ec335241518e99f69
DanteLorenzo <n1emand4me@gmail.com>
2025-06-26 21:27:16 +0300

    test

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -8,7 +8,7 @@ log_info "Installing base packages with pacman..."
 sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift firefox pipewire wireplumber pipewire-pulse pipewire-alsa
 
 # Fonts
-sudo pacman -S ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
+sudo pacman -S --noconfirm ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
 
 if [ $? -eq 0 ]; then
   log_success "Base packages installation complete."
