8efc0732f4de37e5e897ebf8c791427de6aab527
DanteLorenzo <n1emand4me@gmail.com>
2025-06-27 20:45:53 +0300

    hyprpaper

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -5,7 +5,7 @@ source "$(dirname "$0")/../utils/logging.sh"
 
 # Base
 log_info "Installing base packages with pacman..."
-sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift firefox pipewire wireplumber pipewire-pulse pipewire-alsa xorg-xkbcomp jq
+sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift firefox pipewire wireplumber pipewire-pulse pipewire-alsa pavucontrol xorg-xkbcomp jq
 
 # Fonts
 sudo pacman -S --needed --noconfirm ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
---
+++ scripts/04-folders.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Include logging functions
+source "$(dirname "$0")/../utils/logging.sh"
+
+# Base
+log_info "Installing base packages with pacman..."
+
+
+# List of folders to create
+folders=("Downloads" "Music" "Pictures" "Movies" "Projects")
+
+for folder in "${folders[@]}"; do
+    if [ ! -d ~/$folder ]; then
+        mkdir ~/$folder
+        log_success "Created folder: $folder"
+    else
+        log_warning "Folder already exists: $folder"
+    fi
+done
+
+# Copy all files from ./pic to ~/Pictures
+if [ -d ./pic ]; then
+    cp -r ./pic/* ~/Pictures/
+    log_success "Copied all files from ./pic to ~/Pictures"
+else
+    log_error "Source folder ./pic does not exist"
+fi
+
