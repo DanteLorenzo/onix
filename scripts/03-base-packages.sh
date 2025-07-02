ff88c7afa7c623a5cd59ef075a69f309a87ccfac
DanteLorenzo <n1emand4me@gmail.com>
2025-07-02 16:56:28 +0300

    blue

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -5,7 +5,10 @@ source "$(dirname "$0")/../utils/logging.sh"
 
 # Base
 log_info "Installing base packages with pacman..."
-sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift xorg-xkbcomp jq btop htop networkmanager
+sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift xorg-xkbcomp jq btop htop networkmanager bluez bluez-utils blueman
+
+# Start bluetooth
+sudo systemctl enable bluetooth.service --now
 
 # Fonts
 sudo pacman -S --needed --noconfirm ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji ttf-font-hack
