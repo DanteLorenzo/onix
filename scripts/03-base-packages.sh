65163b95de77b39e686b88e1caaa05a63f693d53
DanteLorenzo <n1emand4me@gmail.com>
2025-07-02 16:15:30 +0300

    dunst + alacritty

--- scripts/03-base-packages.sh
+++ scripts/03-base-packages.sh
@@ -5,7 +5,7 @@ source "$(dirname "$0")/../utils/logging.sh"
 
 # Base
 log_info "Installing base packages with pacman..."
-sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift xorg-xkbcomp jq btop htop
+sudo pacman -S --needed --noconfirm base-devel gcc cmake git mesa gdb nano vim ninja vulkan-radeon timeshift xorg-xkbcomp jq btop htop networkmanager
 
 # Fonts
 sudo pacman -S --needed --noconfirm ttf-nerd-fonts-symbols ttf-font-awesome noto-fonts noto-fonts-emoji
