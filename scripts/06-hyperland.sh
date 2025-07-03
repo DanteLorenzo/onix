fa7f2181c16e0aeaf8977fcb8028516cee7a1df0
DanteLorenzo <n1emand4me@gmail.com>
2025-07-03 22:53:43 +0300

    rofi

--- scripts/06-hyperland.sh
+++ scripts/06-hyperland.sh
@@ -4,7 +4,7 @@
 source "$(dirname "$0")/../utils/logging.sh"
 
 log_info "Installing Hyprland and dependencies with pacman..."
-sudo pacman -Sy --needed --noconfirm hyprland kitty waybar hyprpaper bc brightnessctl dunst libnotify pavucontrol alacritty nautilus fastfetch hyprlock hyprcursor hypridle sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg
+sudo pacman -Sy --needed --noconfirm hyprland kitty waybar hyprpaper bc brightnessctl dunst libnotify pavucontrol alacritty nautilus fastfetch hyprlock hyprcursor hypridle sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg rofi-wayland
 # libxcb xcb-proto xcb-util libx11 libxfixes libxcomposite xorg-xinput libxrender wayland-protocols xcb-util-keysyms xcb-util-wm cairo polkit meson seatd libxkbcommon
 
 #   wofi
